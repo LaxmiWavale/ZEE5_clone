ce36db128e7768de475d10a129f0edebca5bcf90
tanuk11 <101566204+tanuk11@users.noreply.github.com>
2022-04-03 22:50:37 +0530

    Add files via upload

---
+++ Web-Series.js
@@ -0,0 +1,593 @@
+
+var Data1=[
+   
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z596111/portrait/1920x7700af155a1127546388a7211ded20785b3f95d4f9a653346c3959058506140089a.jpg",
+   name:"Bloody Brothers",
+   },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z587408/portrait/1920x770195db875fe204fa4afe115764b5dc00ba004028948aa4211b107faed649301d7.jpg",
+  name:"Mithya",
+  },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-1298/portrait/1920x770a6a465c0469e4895bee00d86a4d32d17.jpg",
+  name:"Abhay",
+   },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z597039/portrait/1920x7708ac7dc91daf34fd390f8c9042bf8ec42.jpg",
+  name:"Sutliyan", 
+   },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z560880/portrait/1920x77049b3662989f0475198d1963a5d256ebc.jpg",
+  name:"Kaun Banegi Shikharvati",
+  },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-2955/portrait/1920x7702bde888e01a54a1688ff0a84809a657aef9692fca19245ee980762d8461cfb17.jpg",
+  name:"Jeet ki Zid",
+   },
+  ]
+
+var Data2=[
+    
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z5117885/portrait/1920x770850c8dc6db2248dbaec6959a0006b504.jpg",
+ name:"Modern Parivaar",
+},
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z5110110/portrait/1920x77045f0752c17aa41ae90886e8114730e17.jpg",
+ name:"Imperfect",
+},
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z597457/portrait/1920x7708241f64a47614028ba207e94aef2c536.jpg",
+ name:"Mom & Co.",
+},
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z592284/portrait/1920x77023ee205d04a3430192ec12d7bcbb166f.jpg",
+ name:"Happily Ever After",
+},
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z587457/portrait/ready2mingle1920x770498ef3bcc9ed4fcdb39ceea19912ad0f.jpg",
+ name:"Ready To Mingle",
+},
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-4z571927/portrait/1920x7706cb3f7f4df804ed2847803f0129cf324.jpg",
+ name:"The Holiday",
+}
+]
+
+var Data3=[
+ 
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-2080/portrait/062080incover598958304.jpg",
+  name:"Avhay",
+  },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-1377/portrait/061377incover11350900031135251fe386721a4425b0a4f6b2cc00542f.jpg",
+  name:"Black Widows",
+  },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-1160_34836872/portrait/1920x770in182137154518213715f10a8bc3da8142a3bdcf8030b38c7569.jpg",
+  name:"His Story",
+  },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-1960/portrait/061960incover359151053591f914ce2a9356420ebe8d148f5d756973.jpg",
+  name:"Mum Bhai",
+   },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-1527/portrait/061527incover.jpg",
+  name:"Bichho ka Khel",
+ },
+  {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-tvshow_1473145801/portrait/06tvshow1473141024913588.jpg" ,
+  name:"Taish",
+ },
+  ]
+
+var Data4=[
+    
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-1298/portrait/1920x770a6a465c0469e4895bee00d86a4d32d17.jpg",
+ name:"Brothers",
+ },
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-3029/portrait/063029incover83682024583685789567ca29344c3afa58ccd14134d56.jpg",
+ name:"Mrs. & Mr. Shameem",
+ },
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-3299/portrait/063299incover.jpg",
+ name:"Family",
+ },
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-3051/portrait/1920x770in1063538472.jpg",
+ name:"Maithya",
+ },
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-3064/portrait/1920x770f6f1d5315e9145808275b6b69e6572f7.jpg",
+ name:"Pavitra Rishta 2.0",
+},
+ {image:"https://akamaividz2.zee5.com/image/upload/w_195,h_293,c_scale,f_webp,q_auto:eco,dpr_2.0/resources/0-6-3044/portrait/063044incover98740071191a4d21bb818ea29d68541e.jpg",
+ name:"Kaun Banegi Shikharwati",
+ }
+]
+
+
+var Data5=[
+    
+{image:"https://akamaividz2.zee5.com/image/upload/platform/lang/hindi.jpg",
+
+text:"Hindi",
+},
+{image:"https://akamaividz2.zee5.com/image/upload/platform/lang/marathi.jpg",
+
+text:"Marathi",
+},
+{image:"https://akamaividz2.zee5.com/image/upload/platform/lang/tamil.jpg",
+
+text:"Tamil",
+},
+{image:"https://akamaividz2.zee5.com/image/upload/platform/lang/telugu.jpg",
+
+text:"Telugu",
+},
+{image:"https://akamaividz2.zee5.com/image/upload/platform/lang/bengali.jpg",
+
+text:"Bengali",
+},
+// {image:"https://akamaividz2.zee5.com/image/upload/platform/lang/kannada.jpg",
+
+// text:"Kannada",
+// }
+]
+
+
+var Data6=[
+    
+{image:"https://akamaividz2.zee5.com/image/upload/platform/original/Crime-genre.jpg",
+
+text:"Crime",
+},
+{image:"https://akamaividz2.zee5.com/image/upload/platform/original/Thriller-genre.jpg",
+
+text:"Thrillar",
+},
+{image:"https://akamaividz2.zee5.com/image/upload/platform/original/Comedy-genre.jpg",
+
+text:"Comedy",
+},
+{image:"https://akamaividz2.zee5.com/image/upload/platform/original/romantic.jpg",
+
+text:"Romantic",
+},
+{image:"https://akamaividz2.zee5.com/image/upload/platform/original/Action-genre.jpg",
+
+text:"Action",
+},
+]
+
+    Data1.map(function (elem) {
+    var box = document.createElement("div");
+    box.setAttribute("class", "box")
+
+    var prebtn=document.createElement("button")
+    // prebtn.tagName=<i class="fa-solid fa-crown"></i>
+    prebtn.setAttribute("class","prebtn")
+
+    var img2 = document.createElement("img");
+    img2.src = elem.image;
+    img2.setAttribute("class","logo")
+
+   
+   var rowbtn= document.createElement("div");
+   rowbtn.setAttribute("class", "rowwatch")
+
+   var showname= document.createElement("p")
+   showname.innerText= elem.name
+   showname.setAttribute("class", "shownames")
+
+
+   var posbtn=document.createElement("div");
+   posbtn.setAttribute("class" ,"posbtn")
+
+  var Watchbtn= document.createElement("div")
+  Watchbtn.innerText="Watch"
+  // Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+  Watchbtn.setAttribute("class", "watchBtn")
+  Watchbtn.addEventListener("click",function(e){
+    move(e);
+})
+
+  var sharebtn=document.createElement("div")
+  sharebtn.innerText="share";
+  sharebtn.setAttribute("class" ,"sharebtn")
+
+  posbtn.append(Watchbtn,sharebtn)
+
+  rowbtn.append(showname,posbtn)
+
+    box.append(prebtn,img2,rowbtn);
+
+    document.querySelector(".row2").append(box);
+  });
+
+
+    
+
+  Data2.map(function (elem) {
+    var box = document.createElement("div");
+    box.setAttribute("class", "box")
+
+    var img2 = document.createElement("img");
+    img2.src = elem.image;
+    img2.setAttribute("class","logo")
+
+   
+   var rowbtn= document.createElement("div");
+   rowbtn.setAttribute("class", "rowwatch")
+
+   var showname= document.createElement("p")
+   showname.innerText= elem.name
+   showname.setAttribute("class", "shownames")
+
+   var posbtn=document.createElement("div");
+   posbtn.setAttribute("class" ,"posbtn")
+
+  var Watchbtn= document.createElement("div")
+  Watchbtn.innerText="Watch"
+    Watchbtn.addEventListener("click",function(e){
+    move(e);
+})
+  // Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+  Watchbtn.setAttribute("class", "watchBtn")
+
+  var sharebtn=document.createElement("div")
+  sharebtn.innerText="share";
+  sharebtn.setAttribute("class" ,"sharebtn")
+
+  posbtn.append(Watchbtn,sharebtn)
+
+  rowbtn.append(showname,posbtn)
+
+    box.append(img2,rowbtn);
+
+    document.querySelector(".row3").append(box);
+  });
+
+  Data3.map(function (elem) {
+    var box = document.createElement("div");
+    box.setAttribute("class", "box")
+
+    var img2 = document.createElement("img");
+    img2.src = elem.image;
+    img2.setAttribute("class","logo")
+
+   
+   var rowbtn= document.createElement("div");
+   rowbtn.setAttribute("class", "rowwatch")
+
+   var showname= document.createElement("p")
+   showname.innerText= elem.name
+   showname.setAttribute("class", "shownames")
+
+  var posbtn=document.createElement("div");
+   posbtn.setAttribute("class" ,"posbtn")
+
+  var Watchbtn= document.createElement("div")
+  Watchbtn.innerText="Watch"
+  Watchbtn.addEventListener("click",function(e){
+    move(e);
+})
+  // Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+  Watchbtn.setAttribute("class", "watchBtn")
+
+  var sharebtn=document.createElement("div")
+  sharebtn.innerText="share";
+  sharebtn.setAttribute("class" ,"sharebtn")
+
+  posbtn.append(Watchbtn,sharebtn)
+
+  rowbtn.append(showname,posbtn)
+    box.append(img2,rowbtn);
+
+    document.querySelector(".row4").append(box);
+  });
+
+  Data4.map(function (elem) {
+    var box = document.createElement("div");
+    box.setAttribute("class", "box")
+
+    var img2 = document.createElement("img");
+    img2.src = elem.image;
+    img2.setAttribute("class","logo")
+
+   
+   var rowbtn= document.createElement("div");
+   rowbtn.setAttribute("class", "rowwatch")
+
+   var showname= document.createElement("p")
+   showname.innerText= elem.name
+   showname.setAttribute("class", "shownames")
+
+   var posbtn=document.createElement("div");
+   posbtn.setAttribute("class" ,"posbtn")
+
+  var Watchbtn= document.createElement("div")
+  Watchbtn.innerText="Watch"
+  Watchbtn.addEventListener("click",function(e){
+    move(e);
+})
+  // Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+  Watchbtn.setAttribute("class", "watchBtn")
+
+  var sharebtn=document.createElement("div")
+  sharebtn.innerText="share";
+  sharebtn.setAttribute("class" ,"sharebtn")
+
+  posbtn.append(Watchbtn,sharebtn)
+
+  rowbtn.append(showname,posbtn)
+
+    box.append(img2,rowbtn);
+
+    document.querySelector(".row5").append(box);
+  });
+
+
+  Data5.map(function (elem) {
+    var box = document.createElement("div");
+    box.setAttribute("class", "box")
+
+    var img2 = document.createElement("img");
+    img2.src = elem.image;
+    img2.setAttribute("class","logo")
+
+    var text=document.createElement("div");
+    text.innerText=elem.text;
+    text.setAttribute("class","text")
+
+   
+   var rowbtn= document.createElement("div");
+   rowbtn.setAttribute("class", "rowwatch")
+
+  //  var showname= document.createElement("p")
+  //  showname.innerText= elem.name
+  //  showname.setAttribute("class", "shownames")
+
+   var posbtn=document.createElement("div");
+   posbtn.setAttribute("class" ,"posbtn")
+
+  var Watchbtn= document.createElement("div")
+  Watchbtn.innerText="Watch"
+  Watchbtn.addEventListener("click",function(e){
+    move(e);
+})
+  // Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+  Watchbtn.setAttribute("class", "watchBtn")
+
+  var sharebtn=document.createElement("div")
+  sharebtn.innerText="share";
+  sharebtn.setAttribute("class" ,"sharebtn")
+
+  posbtn.append(Watchbtn,sharebtn)
+
+  rowbtn.append(posbtn)
+
+    box.append(img2,text,rowbtn);
+
+    document.querySelector(".row6").append(box);
+  });
+
+
+  Data6.map(function (elem) {
+    var box = document.createElement("div");
+    box.setAttribute("class", "box")
+
+    var img2 = document.createElement("img");
+    img2.src = elem.image;
+    img2.setAttribute("class","logo")
+
+    var text=document.createElement("div");
+    text.innerText=elem.text;
+    text.setAttribute("class","text")
+
+   
+   var rowbtn= document.createElement("div");
+   rowbtn.setAttribute("class", "rowwatch")
+
+  //  var showname= document.createElement("p")
+  //  showname.innerText= elem.name
+  //  showname.setAttribute("class", "shownames")
+
+   var posbtn=document.createElement("div");
+   posbtn.setAttribute("class" ,"posbtn")
+
+  var Watchbtn= document.createElement("div")
+  Watchbtn.innerText="Watch"
+  Watchbtn.addEventListener("click",function(e){
+    move(e);
+})
+  // Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+  Watchbtn.setAttribute("class", "watchBtn")
+
+  var sharebtn=document.createElement("div")
+  sharebtn.innerText="share";
+  sharebtn.setAttribute("class" ,"sharebtn")
+
+  posbtn.append(Watchbtn,sharebtn)
+
+  rowbtn.append(posbtn)
+
+    box.append(img2,text,rowbtn);
+
+    document.querySelector(".row7").append(box);
+  });
+
+  function searching(){
+    document.querySelector("#submenu1").innerHTML=""
+    var input=document.querySelector("#searchMv").value
+    if(true){
+      var searched=Data1.filter(function(e){
+          if(e.name==input){
+              return e.name;
+          }
+    })
+    console.log(searched)
+    searched.map(function (e){
+      var box = document.createElement("div");
+      box.setAttribute("class", "box")
+      var prebtn=document.createElement("button")
+      // prebtn.tagName=<i class="fa-solid fa-crown"></i>
+      prebtn.setAttribute("class","prebtn")
+      var img2 = document.createElement("img");
+      img2.src = e.image;
+      img2.setAttribute("class","logo")
+      var rowbtn= document.createElement("div");
+      rowbtn.setAttribute("class", "rowwatch")
+      var showname= document.createElement("p")
+      showname.innerText= e.name
+      showname.setAttribute("class", "shownames")
+      var posbtn=document.createElement("div");
+      posbtn.setAttribute("class" ,"posbtn")
+      var Watchbtn= document.createElement("div")
+      Watchbtn.innerText="Watch"
+  // Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+     Watchbtn.setAttribute("class", "watchBtn")
+     Watchbtn.addEventListener("click",function(e){
+      move(e);
+      })
+      var sharebtn=document.createElement("div")
+      sharebtn.innerText="share";
+      sharebtn.setAttribute("class" ,"sharebtn")
+      posbtn.append(Watchbtn,sharebtn)
+      
+      rowbtn.append(showname,posbtn)
+      
+      box.append(prebtn,img2,rowbtn);
+      document.querySelector("#submenu1").append(box)
+        box.addEventListener("click",function(e){
+        move(e);
+       })
+    })
+  }
+
+  if(true){
+    var searched=Data2.filter(function(e){
+        if(e.name==input){
+            return e.name;
+        }
+  })
+  console.log(searched)
+  searched.map(function (e){
+    var box = document.createElement("div");
+    box.setAttribute("class", "box")
+    var prebtn=document.createElement("button")
+    // prebtn.tagName=<i class="fa-solid fa-crown"></i>
+    prebtn.setAttribute("class","prebtn")
+    var img2 = document.createElement("img");
+    img2.src = e.image;
+    img2.setAttribute("class","logo")
+    var rowbtn= document.createElement("div");
+    rowbtn.setAttribute("class", "rowwatch")
+    var showname= document.createElement("p")
+    showname.innerText= e.name
+    showname.setAttribute("class", "shownames")
+    var posbtn=document.createElement("div");
+    posbtn.setAttribute("class" ,"posbtn")
+    var Watchbtn= document.createElement("div")
+    Watchbtn.innerText="Watch"
+// Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+   Watchbtn.setAttribute("class", "watchBtn")
+   Watchbtn.addEventListener("click",function(e){
+    move(e);
+    })
+    var sharebtn=document.createElement("div")
+    sharebtn.innerText="share";
+    sharebtn.setAttribute("class" ,"sharebtn")
+    posbtn.append(Watchbtn,sharebtn)
+    
+    rowbtn.append(showname,posbtn)
+    
+    box.append(prebtn,img2,rowbtn);
+    document.querySelector("#submenu1").append(box)
+      box.addEventListener("click",function(e){
+      move(e);
+     })
+  })
+}
+
+if(true){
+  var searched=Data3.filter(function(e){
+      if(e.name==input){
+          return e.name;
+      }
+})
+console.log(searched)
+searched.map(function (e){
+  var box = document.createElement("div");
+  box.setAttribute("class", "box")
+  var prebtn=document.createElement("button")
+  // prebtn.tagName=<i class="fa-solid fa-crown"></i>
+  prebtn.setAttribute("class","prebtn")
+  var img2 = document.createElement("img");
+  img2.src = e.image;
+  img2.setAttribute("class","logo")
+  var rowbtn= document.createElement("div");
+  rowbtn.setAttribute("class", "rowwatch")
+  var showname= document.createElement("p")
+  showname.innerText= e.name
+  showname.setAttribute("class", "shownames")
+  var posbtn=document.createElement("div");
+  posbtn.setAttribute("class" ,"posbtn")
+  var Watchbtn= document.createElement("div")
+  Watchbtn.innerText="Watch"
+// Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+ Watchbtn.setAttribute("class", "watchBtn")
+ Watchbtn.addEventListener("click",function(e){
+  move(e);
+  })
+  var sharebtn=document.createElement("div")
+  sharebtn.innerText="share";
+  sharebtn.setAttribute("class" ,"sharebtn")
+  posbtn.append(Watchbtn,sharebtn)
+  
+  rowbtn.append(showname,posbtn)
+  
+  box.append(prebtn,img2,rowbtn);
+  document.querySelector("#submenu1").append(box)
+    box.addEventListener("click",function(e){
+    move(e);
+   })
+})
+}
+
+if(true){
+  var searched=Data4.filter(function(e){
+      if(e.name==input){
+          return e.name;
+      }
+})
+console.log(searched)
+searched.map(function (e){
+  var box = document.createElement("div");
+  box.setAttribute("class", "box")
+  var prebtn=document.createElement("button")
+  // prebtn.tagName=<i class="fa-solid fa-crown"></i>
+  prebtn.setAttribute("class","prebtn")
+  var img2 = document.createElement("img");
+  img2.src = e.image;
+  img2.setAttribute("class","logo")
+  var rowbtn= document.createElement("div");
+  rowbtn.setAttribute("class", "rowwatch")
+  var showname= document.createElement("p")
+  showname.innerText= e.name
+  showname.setAttribute("class", "shownames")
+  var posbtn=document.createElement("div");
+  posbtn.setAttribute("class" ,"posbtn")
+  var Watchbtn= document.createElement("div")
+  Watchbtn.innerText="Watch"
+// Watchbtn.innerText=<i class="fa-solid fa-play"></i>
+ Watchbtn.setAttribute("class", "watchBtn")
+ Watchbtn.addEventListener("click",function(e){
+  move(e);
+  })
+  var sharebtn=document.createElement("div")
+  sharebtn.innerText="share";
+  sharebtn.setAttribute("class" ,"sharebtn")
+  posbtn.append(Watchbtn,sharebtn)
+  
+  rowbtn.append(showname,posbtn)
+  
+  box.append(prebtn,img2,rowbtn);
+  document.querySelector("#submenu1").append(box)
+    .box.addEventListener("click",function(e){
+    move(e);
+   })
+})
+}
+  
+  }
+
+
+  function move(e){
+    window.location.href="playing.html"
+  }
+
+  function movetobuy(e){
+    window.location.href="buyplan.html"
+}
